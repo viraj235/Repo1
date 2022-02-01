aeb719a4ee38c834afba55c94fe65872730465fb
viraj235 <shashiraj4991@email.com>
2022-02-01 16:18:55 +0530

    am adding Addition file

---
+++ Addition.java
@@ -0,0 +1,21 @@
+class Addition
+{
+
+public static void main(String[] args)
+{
+System.out.println("I am starting Additin of two numbers");
+
+int a=20;
+int b=40;
+int c=a+b;
+System.out.println(c);
+
+}
+
+}
+
+
+Substr 2
+
+mutl 3 number 
+Diviton 2 num
